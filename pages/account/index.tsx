b1e088b45be8e04c4a7ebd3421b63c80ce1c4446
sungzzn <msj9121@naver.com>
2023-07-15 18:28:43 +0900

    [feat] 계정 관리 페이지 생성

---
+++ pages/account/index.tsx
@@ -0,0 +1,11 @@
+import { ReactElement, useEffect } from "react";
+import { NextPage } from "next";
+import {} from "next/router";
+
+import AccountTemplate from "../../templates/Account";
+
+function Account() {
+  return <AccountTemplate />;
+}
+
+export default Account;
---
+++ templates/Account/index.tsx
@@ -0,0 +1,16 @@
+import styled from "@emotion/styled";
+
+import SignInlogo from "../../assets/icons/SignInlogo.svg";
+
+import Typography from "../../components/Typography/Pretendard";
+import SolidButton from "../../components/Button/Solid";
+
+interface Props {}
+
+function Account() {
+  return <Template>Account</Template>;
+}
+
+const Template = styled.div``;
+
+export default Account;
